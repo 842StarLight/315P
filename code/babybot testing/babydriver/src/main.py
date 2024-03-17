0a4f2012fe388bf48f2437899792fffa1e0c56f6
842StarLight <842starlite@gmail.com>
2024-03-16 17:56:50 -0700

    Sheeeeeeeeeeeeeeeeeeeeeesh
    One 'e' in the sheesh for every time I squealed or groaned today
    
    On the plus side:
    1. Babybot drivetrain and driver controls work
    2. PD turn2 works on babybot
    On the downside:
    3. No sensors implemented yet
    4. We don't talk about PID drive4

--- code/babybot testing/babydriver/src/main.py
+++ code/babybot testing/babydriver/src/main.py
@@ -1,5 +1,4 @@
 from vex import *
-import urandom
 import time
 import math
 import random
@@ -11,27 +10,49 @@ brain = Brain()
 # controller
 controller_1 = Controller(PRIMARY)
 # sensors - MAIN PART HERE
-# orientation = Inertial(Ports.PORT1)
-# orientation.calibrate()
-# while orientation.is_calibrating():
-#     wait(10, MSEC)
+orientation = Inertial(Ports.PORT10)
+orientation.calibrate()
+while orientation.is_calibrating():
+    wait(10, MSEC)
 
 # drivetrain
-dt_left = MotorGroup(
-    Motor(Ports.PORT1, GearSetting.RATIO_18_1, True)
-)
-dt_right = MotorGroup(
-    Motor(Ports.PORT20, GearSetting.RATIO_18_1, False)
-)
+dt_right = MotorGroup(Motor(Ports.PORT1, GearSetting.RATIO_18_1, True))
+dt_left = MotorGroup(Motor(Ports.PORT4, GearSetting.RATIO_18_1, False))
+drive = SmartDrive(dt_left, dt_right, orientation)
 dt_left.set_stopping(BRAKE) 
 dt_right.set_stopping(BRAKE)
+class PID:
+    def __init__(self, sample_rate, initial_error, consts): # sample_rate in seconds
+        self.sample_rate = sample_rate
+        self.kP = consts[0]
+        self.kI = consts[1]
+        self.kD = consts[2]
+
+        self.integral = 0.0  # Initialize integral for error accumulation
+        self.prev_error = initial_error  # Initialize previous error for derivative calculation
+
+    def update(self, error):
+        # Proportional term
+        proportional = self.kP * error
+
+        # Integral term
+        self.integral += error*self.sample_rate  # Accumulate error over time
+        integral = self.kI * self.integral
+
+        # Derivative term
+        derivative = self.kD * (error - self.prev_error) / self.sample_rate
+        self.prev_error = error  # Update previous error for next calculation
+
+        # Calculate intended velocity
+        velocity = proportional + integral + derivative
+
+        return velocity
 
 class Drivetrain:
     def __init__(self, gear_ratio, wheel_diameter, wheelbase):
         self.gear_ratio = gear_ratio
         self.wheel_diameter = wheel_diameter
         self.wheelbase = wheelbase
-        self.dt_time = 0
     def drive4(self, inches, speed=200, timeout=1):
         first = brain.timer.time(SECONDS)
         '''
@@ -54,33 +75,31 @@ class Drivetrain:
         # speeds
         dt_left.set_velocity(speed, PERCENT)
         dt_right.set_velocity(speed, PERCENT)
+        # consts
+        dt_const = self.gear_ratio/(math.pi*self.wheel_diameter)
+        #12.5in per revolution
+        turns = dt_const * inches
         # core spin
-        dt_left.spin_for(FORWARD, (inches/(math.pi*self.wheel_diameter))*self.gear_ratio, TURNS, wait=False)
-        dt_right.spin_for(FORWARD, (inches/(math.pi*self.wheel_diameter))*self.gear_ratio, TURNS, wait=False)
+        dt_left.spin_for(FORWARD, dt_const*inches, TURNS, wait=False)
+        dt_right.spin_for(FORWARD, dt_const*inches, TURNS, wait=False)
         # record initial time; var to check if dt has accelerated
         initial_time = brain.timer.time(SECONDS)
-        is_started = False
         running = True
-        # main timeout loop
+        # wait until started
+        while dt_left.velocity(PERCENT) <= 5:
+            wait(10, MSEC)
+        # main loop
         while running:
             wait(10, MSEC)
-            # is_started makes sure we don't stop during accel
-            if dt_left.velocity(PERCENT) > 5:
-                is_started = True
-            # main checks
-            if brain.timer.time(SECONDS)-initial_time >= timeout:
-                print("drive4 timed out: dist", inches, "timeout", timeout)
+            if brain.timer.time(SECONDS) - initial_time > timeout:
                 running = False
-            elif is_started and dt_left.velocity(PERCENT) == 0: # UTB if
+            elif dt_left.velocity(PERCENT) == 0:
                 # recalculate as a form of error filtering
                 wait(40, MSEC)
                 running = not (dt_left.velocity(PERCENT) == 0)
         # stop
-        dt_left.stop()
-        dt_right.stop()
+        drive.stop()
         # logging
-        #print("drive4/done", inches, "in, took", brain.timer.time(SECONDS)-initial_time, 'sec')
-        self.dt_time += brain.timer.time(SECONDS)-first
     def turn2(self, angle_unmodded, speed=41):
         '''
         ### A turn-to-heading function which uses a feedback loop (just P for now) in tandem with the inertial to achieve very precise results.
@@ -150,8 +169,8 @@ class Drivetrain:
         sconst = speed/(abs(left)/2+abs(right)/2) # to add the speed factor
         print('fastarc', rad, head, side, duration, speed)
         # and away she goes!
-        dt_right.spin(FORWARD, right*sconst*12/100, VOLT)
-        dt_left.spin(FORWARD, left*sconst*12/100, VOLT)
+        dt_right.spin(FORWARD, right*sconst*12/100, VoltageUnits.VOLT) # type: ignore
+        dt_left.spin(FORWARD, left*sconst*12/100, VoltageUnits.VOLT) # type: ignore
         if not finish:
             print(left*sconst, right*sconst)
             return None
@@ -169,22 +188,172 @@ class Drivetrain:
             dt_left.stop()
             dt_right.stop()
             print(brain.timer.time(SECONDS)-initial_time)
-        '''
-        
-       
+        '''     
 dt = Drivetrain(1, 4, 11)
+
+# voltage utility
+def clamp_volt(volt):
+    if volt > 0:
+        volt = min(12, volt)
+        volt = max(3, volt)
+    elif volt < 0:
+        volt = max(-12, volt)
+        volt = min(-3, volt)
+    return volt
 # driver control
-def driver_control():
+def driver_control(time=10**10):
     # EXTREMELY dumbed-down
-    while True:
-        wait(10, MSEC)
+    initial_time = brain.timer.time(SECONDS)
+    while brain.timer.time(SECONDS) - initial_time <= time:
+        wait(2, MSEC)
         # dt controls
         straight_speed = controller_1.axis3.position()
-        turn_speed = 0.5*controller_1.axis1.position()
+        turn_speed = 0.75*controller_1.axis1.position()
         l = straight_speed + turn_speed
         r = straight_speed - turn_speed
-        dt_left.spin((FORWARD if l >= 0 else REVERSE), abs(l)*12/100, VOLT)
-        dt_right.spin((FORWARD if r >= 0 else REVERSE), abs(r)*12/100, VOLT)
-def autonomous():
-    pass
-competition = Competition(driver_control, autonomous)
+        dt_left.spin((FORWARD if l >= 0 else REVERSE), abs(l)*12/100, VoltageUnits.VOLT) # type: ignore
+        dt_right.spin((FORWARD if r >= 0 else REVERSE), abs(r)*12/100, VoltageUnits.VOLT) # type: ignore
+    dt_left.stop()
+    dt_right.stop()
+# pid turn function
+def pid_turn(angle_unmodded, c=(2, 0, 0), logs=False):
+    # error calc fn
+    angle = angle_unmodded % 360
+    def error_calc(d):
+        abs_err = (angle-d)%360 # 0 to 360
+        norm_err = ((-abs_err % -180)+(180 if abs_err>=180 else 0)) # -180 to 180
+        return norm_err/180 # -1 to 1
+    # basic vars & controller setup
+    wait_time = 0.01
+    err = error_calc(orientation.heading(DEGREES))
+    control = PID(wait_time, err, c)
+    # logs stuff
+    vels = []
+    itime = brain.timer.time(SECONDS)
+    while (abs(err) >= 0.01 or abs(dt_left.velocity(PERCENT)) >= 5) and brain.timer.time(SECONDS)-itime <= 5:
+        # speed calcs
+        err = error_calc(orientation.heading(DEGREES))
+        target = control.update(err)*100
+        volt = clamp_volt(target*12/100)
+        # spin ahoy!
+        dt_left.spin(REVERSE, volt, VoltageUnits.VOLT) # type: ignore
+        dt_right.spin(FORWARD, volt, VoltageUnits.VOLT) # type: ignore
+        # vels
+        vels.append((target, -dt_left.velocity(PERCENT) ))
+        # 10 msec wait
+        wait(wait_time*1000, MSEC)
+    dt_left.stop()
+    dt_right.stop()
+    if logs:
+        print('\n'.join([' '.join([str(j) for j in i]) for i in vels]))
+class TurnTest: # comprehensive class in which we can write code to test a variety of turn functions
+    def __init__(self, fn_wrapper):
+        # fn_wrapper - a function instance which takes in an angle then turns to it
+        self.fn = fn_wrapper
+    def comprehensive(self, to=180):
+        times = []
+        for degrees in range(10, to+1, 10):
+            wait(2, SECONDS)
+
+            orientation.set_heading(0, DEGREES)
+
+            brain.timer.clear()
+            self.fn(degrees)
+            times.append(brain.timer.time(SECONDS)/2)
+            brain.timer.clear()
+            self.fn(0)
+            times[-1] += brain.timer.time(SECONDS)/2
+
+        print(', '.join([str(round(i,3)) for i in times]))
+    def single(self, a=90):
+        orientation.set_heading(0, DEGREES)
+        brain.timer.clear()
+        self.fn(a)
+        print(brain.timer.time(MSEC))
+        wait(0.5, SECONDS)
+        print(orientation.heading(DEGREES))
+def turn_wrap(*consts, log=False):
+    def wrapper(a):
+        pid_turn(a, c=consts, logs=log)
+    return TurnTest(wrapper)
+# pid drive
+def pid_drive(inches, c=(2, 0, 0), speed=100, timeout=5, log=False):
+    # reset
+    dt_left.reset_position()
+    dt_right.reset_position()
+    initial_time = brain.timer.time(SECONDS)
+    # error calc
+    dt_const = dt.gear_ratio/(math.pi*dt.wheel_diameter)
+    def error_calc(encoder_val):
+        # convert rotations to inches
+        turns = inches*dt_const
+        return (turns-encoder_val)/abs(turns)
+    # set up error & controllers
+    wait_time = 0.01
+    left_err = error_calc(dt_left.position(TURNS)/0.7233334)
+    right_err = error_calc(dt_right.position(TURNS))
+    left_control = PID(wait_time, left_err, c)
+    right_control = PID(wait_time, right_err, c)
+    buffer = ""
+    while (
+        (abs(left_err) >= 0.01) or (abs(right_err) >= 0.01) 
+        #or
+        #(dt_left.velocity(PERCENT) > 5) or (dt_right.velocity(PERCENT) > 5)
+    ) and brain.timer.time(SECONDS)-initial_time <= timeout:
+        left_err = error_calc(dt_left.position(TURNS)/0.7233334)
+        right_err = error_calc(dt_right.position(TURNS))
+        # calculate target velocities
+        """left_target = clamp_volt(
+            left_control.update(
+                left_err
+            ) * speed * 12/100
+        )
+        right_target = clamp_volt(
+            right_control.update(
+                right_err
+            )  * speed * 12/100
+        )"""
+        left_target = left_control.update(left_err) * speed
+        right_target = right_control.update(right_err)  * speed
+        buffer += str(left_target) + " " + str(left_err) + " " + str(dt_left.velocity(PERCENT)) + "\n"
+        # spin ahoy!
+        #dt_left.spin(FORWARD if left_target >= 0 else REVERSE, left_target, VoltageUnits.VOLT) # type: ignore
+        #dt_right.spin(FORWARD if right_target >= 0 else REVERSE, right_target, VoltageUnits.VOLT) # type: ignore
+        dt_left.spin(FORWARD, left_target, PERCENT) # type: ignore
+        dt_right.spin(FORWARD, right_target, PERCENT) # type: ignore
+        # 10 msec wait
+        wait(wait_time*1000, MSEC)
+    dt_left.stop()
+    dt_right.stop()
+    if log:
+        print(buffer)
+class DriveTest: # comprehensive class in which we can write code to test a variety of drive functions
+    def __init__(self, fn_wrapper):
+        # fn_wrapper - a function instance which takes in an distance then drives to it
+        self.fn = fn_wrapper
+    # def comprehensive(self): - how would we implement this?
+    def single(self, dist=24):
+        orientation.set_heading(0, DEGREES)
+        brain.timer.clear()
+        self.fn(dist)
+        print(brain.timer.time(MSEC))
+def drive_wrap(*consts, log=False, speed=100):
+    def wrapper(dist):
+        pid_drive(dist, c=consts, speed=speed, log=log)
+    return DriveTest(wrapper)
+
+"""
+EXPERIMENTS:
+drive4: switch to PID
+ - go-to: drive_wrap(2, .5, .5, log=True).single()
+ - figure it the hell out!
+FINAL turn2: switch to PD
+ - constants kP = 3, kI = 0, kD = 0.2 go-to: turn_wrap(2, 0.5, 0.5).single()
+ - test results below
+ - integrate back into drivetrain class and put testing code into separate file
+"""
+"""
+Test results (angles from 10 to 180, incrementing by 10; average of two turns to get time per angle) (pid constants 3, 0, 0.2)
+turn_pid: 0.28, 0.46, 0.47, 0.5, 0.59, 0.58, 0.54, 0.515, 0.565, 0.565, 0.57, 0.64, 0.61, 0.67, 1.221, 0.73, 0.95, 0.805
+dt.turn2: 0.545, 0.56, 0.6, 0.695, 0.74, 0.985, 1.065, 1.205, 1.165, 1.235, 1.23, 1.271, 1.335, 1.375, 1.39, 1.415, 1.465, 1.66
+"""
